db52bd314bec0aa07e7ac9ed1903b867b4140051
石显军 <1092207960@qq.com>
2019-03-08 09:14:40 +0800

    更新代码

--- XiMaLaYaNav/XiMaLaYaNav/StoneHomeTop/Controller/UIControllerHome.m
+++ XiMaLaYaNav/XiMaLaYaNav/StoneHomeTop/Controller/UIControllerHome.m
@@ -7,10 +7,15 @@
 //
 
 #import "UIControllerHome.h"
+#import "UIViewHomeNav.h"
 
-@interface UIControllerHome ()
+@interface UIControllerHome ()<UITableViewDataSource>
 
 @property (nonatomic, strong) UIView *topBackgroundView;
+
+@property (nonatomic, strong) UIViewHomeNav *homeNav;
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -43,11 +48,23 @@
     self.view.backgroundColor = UIColor.whiteColor;
     
     [self.view addSubview:self.topBackgroundView];
-    
     [self.topBackgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.right.mas_equalTo(self.view);
         make.top.mas_equalTo(-100.f);
         make.height.mas_equalTo(500.f);
+    }];
+    
+    [self.view addSubview:self.homeNav];
+    [self.homeNav mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.mas_equalTo(self.view);
+        make.top.mas_equalTo([UIApplication sharedApplication].statusBarFrame.size.height);
+        make.height.mas_equalTo(44);
+    }];
+    
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.homeNav.mas_bottom);
+        make.left.right.bottom.mas_equalTo(self.view);
     }];
 
 }
@@ -64,5 +81,38 @@
     return _topBackgroundView;
 }
 
+- (UIViewHomeNav *)homeNav
+{
+    if (_homeNav == nil) {
+        _homeNav = [[NSBundle mainBundle] loadNibNamed:UIViewHomeNav.jk_className owner:self options:nil].lastObject;
+    }
+    return _homeNav;
+}
+
+- (UITableView *)tableView
+{
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.dataSource = self;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:UITableViewCell.jk_className];
+        _tableView.rowHeight = 100;
+    }
+    
+    return _tableView;
+}
+
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:UITableViewCell.jk_className];
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld", indexPath.row];
+    return cell;
+}
 
 @end
